0a8a8d43fba61d5a18fdd5f17d4dc04a9d657061
Robert-Rodriguez <robertmasters@hotmail.com>
2021-07-02 21:39:27 -0700

    finished more styling, also added responsive design for large and small desktop, tablet, and mobile sizes

--- mastersportfolio/src/App.js
+++ mastersportfolio/src/App.js
@@ -23,12 +23,15 @@ function App() {
           <div className = "skills-topline2"> </div>
           <div className = "skills-topline3"> </div>
           <SkillsComponent />
-          <div className = "skills-bottomline1"> </div>
-          <div className = "skills-bottomline2"> </div>
+
         </div>
 
-
           <div className = "box3">
+
+          <div className = "intro">
+          Hello I am a software developer currently living in Florida. I am skilled at making websites and android mobile applications from the front end to the back end. This includes: making mobile applications using native technologies such as Java and Kotlin, creating fully functional websites using state management tech such as context or reddux, I can also build fully functional API applications using Spring, but am willing to learn other technologies.
+          </div>
+
             <div className = "featured-label" >Featured Projects</div>
             <img src = {require('./projectDisplayImages/cimg1.jpg').default}  />
             <img src = {require('./projectDisplayImages/cimg3.jpg').default}  />
